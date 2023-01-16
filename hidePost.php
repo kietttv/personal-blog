3c2e05db42659c9277c5580f31cec9a51d066e8c
kietttv <tvtkiet2002@gmail.com>
2023-01-16 11:55:18 +0700

    fix hide post

--- hidePost.php
+++ hidePost.php
@@ -1,26 +1,32 @@
 <?php
-function hidePost($id, $status){
+function hidePost($id, $status)
+{
     include("connect.php");
     include_once("header.php");
     $hidePost = "UPDATE `post` 
                 SET `postStatus` = '$status'
                 WHERE `post`.`id` = $id";
-    if(mysqli_query($conn, $hidePost)){
-        echo"<script>
+    if (mysqli_query($conn, $hidePost)) {
+        echo "<script>
                 window.location = 'index.php';
             </script>";
-    }else{
-        echo "error: ". $hidePost. "<br>". mysqli_errno($conn);
-    } 
+    } else {
+        echo "error: " . $hidePost . "<br>" . mysqli_errno($conn);
+    }
 }
-    include_once("header.php");
-    $user = $_SESSION['user'];
-    if(isset($_SESSION['user'])){
-        if(isset($_GET['id'])){
-            $id = $_GET['id'];
-            hidePost($id, '1');
-        }
-    }else{
-        header("Location: login.php");
-    } 
+include_once("header.php");
+$user = $_SESSION['user'];
+if (isset($_SESSION['user'])) {
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        hidePost($id, '1');
+    } else { ?>
+        <script>
+            window.alert("Something went wrong!!!");
+            window.location = 'index.php';
+        </script>
+<?php }
+} else {
+    header("Location: login.php");
+}
 ?>
